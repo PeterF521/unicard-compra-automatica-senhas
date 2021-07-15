3ba61708cd54d9411cc60915e6d966f93bae93b3
PeterF521 <pedrofelgueiras2006@gmail.com>
2021-07-15 12:05:06 +0100

    Initial application layer

---
+++ src/Application/Services/UsersService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCAR.Domain.Entities;
+using UCAR.Domain.InputDTOs;
+using UCAR.Domain.Interfaces.Services;
+using UCAR.Domain.Interfaces.Repositories;
+using UCAR.Domain.Exceptions;
+
+namespace UCAR.Application.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersService(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public async Task<bool> CheckCredentials(CheckUserCredentialsInputDTO credentials)
+        {
+            User foundUser = await _usersRepository.FindUserByEmail(credentials.Email);
+            if(foundUser == null)
+            {
+                return false;    
+            }
+
+            return foundUser.Password == credentials.Password;
+        }
+
+        public async Task<User> CreateUser(CreateUserInputDTO user)
+        {
+            User foundUser = await _usersRepository.FindUserByEmail(user.Email);
+            if(foundUser != null)
+            {
+                throw new UserAlreadyRegisteredException(foundUser);
+            }
+
+            User userToSave = new()
+            {
+                Name = user.Name,
+                EducatorEmail = user.EducatorEmail,
+                Email = user.Email,
+                Password = user.Password,
+                SignUpDate = DateTime.Now
+            };
+
+            return await _usersRepository.CreateUser(userToSave);
+        }
+    }
+}
